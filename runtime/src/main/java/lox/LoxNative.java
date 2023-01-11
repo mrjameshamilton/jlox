9fb2dc97ca72246254bb8b606b11774d174653dd
James Hamilton <mrjameshamilton@gmail.com>
2023-01-11 22:43:39 +0100

    Add read and b2c native functions

--- runtime/src/main/java/lox/LoxNative.java
+++ runtime/src/main/java/lox/LoxNative.java
@@ -1,7 +1,18 @@
 package lox;
 
+import java.io.IOException;
+
 public class LoxNative {
     public static Object clock() {
         return (double)System.currentTimeMillis() / 1000.0;
+    }
+
+    public static Object read() throws IOException {
+        int b = System.in.read();
+        return b != -1 ? (double)b : null;
+    }
+
+    public static Object b2c(Object o) {
+        return o == null ? null : String.valueOf((char) ((double) o));
     }
 }
