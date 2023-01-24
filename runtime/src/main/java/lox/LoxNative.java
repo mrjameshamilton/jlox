c586dc6b55b3fa7d6fbe6b15ac59f337e19d9a2a
James Hamilton <james.hamilton@guardsquare.com>
2023-01-24 22:09:15 +0100

    Add `utf`, `exit`, `printerr` native functions

--- runtime/src/main/java/lox/LoxNative.java
+++ runtime/src/main/java/lox/LoxNative.java
@@ -2,6 +2,8 @@ package lox;
 
 import java.io.IOException;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class LoxNative {
     public static Object clock() {
         return (double)System.currentTimeMillis() / 1000.0;
@@ -12,7 +14,39 @@ public class LoxNative {
         return b != -1 ? (double)b : null;
     }
 
-    public static Object b2c(Object o) {
-        return o == null ? null : String.valueOf((char) ((double) o));
+    /**
+     * Converts the given bytes into a UTF character String.
+     * The first byte must be non-null.
+     * Called with a single byte is equivalent to ASCII encoding.
+     *
+     * @param o1 Double  First character byte
+     * @param o2 Double? Second character byte
+     * @param o3 Double? Third character byte
+     * @param o4 Double? Fourth character byte
+     * @return A UTF character as a String
+     */
+    public static Object utf(Object o1, Object o2, Object o3, Object o4) {
+        byte[] bytes = new byte[] {
+            o1 == null ? 0 : ((Double)o1).byteValue(),
+            o2 == null ? 0 : ((Double)o2).byteValue(),
+            o3 == null ? 0 : ((Double)o3).byteValue(),
+            o4 == null ? 0 : ((Double)o4).byteValue()
+        };
+        int count = 0;
+        if (o1 != null) count++;
+        if (o2 != null) count++;
+        if (o3 != null) count++;
+        if (o4 != null) count++;
+        return new String(bytes, 0, count, UTF_8);
+    }
+
+    public static Object exit(Object o) {
+        System.exit(((Double)o).intValue());
+        return null;
+    }
+
+    public static Object printerr(Object o) {
+        System.err.println(o);
+        return null;
     }
 }
